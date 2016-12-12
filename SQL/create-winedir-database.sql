4ecb495a665ebb4d1900501e59755eb85aa95f1d
Scott Deeter <scott@deeters.net>
2016-12-11 21:26:48 -0500

    Added backtck quoting for names; rearraged cols

--- SQL/create-winedir-database.sql
+++ SQL/create-winedir-database.sql
@@ -6,24 +6,25 @@ CREATE DATABASE winedir;
 USE winedir;
 
 -- Create the tables.
-CREATE TABLE winemaker
+CREATE TABLE `winemaker`
 (
-  mid int NOT NULL AUTO_INCREMENT,
-  name varchar(45) NOT NULL,
-  description varchar(255),
-  website varchar(100),
-  country int NOT NULL,
-  PRIMARY KEY (mid)
+  `mid` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(255),
+  `website` varchar(100),
+  `country` int NOT NULL,
+  PRIMARY KEY (`mid`)
 ) ENGINE = InnoDB COMMENT='The base table for winemakers.';
 
-CREATE TABLE wine
+CREATE TABLE `wine`
 ( 
-  wid int NOT NULL AUTO_INCREMENT,
-  varietal varchar(100) NOT NULL,
-  name varchar(100),
-  region varchar(100),
-  upc varchar(50),
-  winemaker int NOT NULL,
-  PRIMARY KEY (wid),
-  FOREIGN KEY (winemaker) REFERENCES winemaker(mid)
+  `wid` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(255),
+  `winemaker` int NOT NULL,
+  `varietal` int NOT NULL,
+  `region` int,
+  `upc` varchar(45),
+  PRIMARY KEY (`wid`),
+  FOREIGN KEY (`winemaker`) REFERENCES winemaker(`mid`)
 ) ENGINE = InnoDB COMMENT='The base table for wines.';
