eb166b5e2c71bc4353b0c340b8c6dc0b021040fd
Scott Deeter <scott@deeters.net>
2016-12-11 21:27:34 -0500

    Added varietals table & some default entries

--- SQL/create-winedir-database.sql
+++ SQL/create-winedir-database.sql
@@ -28,3 +28,26 @@ CREATE TABLE `wine`
   PRIMARY KEY (`wid`),
   FOREIGN KEY (`winemaker`) REFERENCES winemaker(`mid`)
 ) ENGINE = InnoDB COMMENT='The base table for wines.';
+
+CREATE TABLE `varietal`
+(
+  `vid` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45),
+  `type` varchar(45),
+  `description` varchar(255),
+  PRIMARY KEY (`vid`)
+) ENGINE = InnoDB COMMENT='The base table for varietals.';
+
+INSERT INTO `varietal` VALUES
+  (null, 'Bordeaux Blend', 'red', null),
+  (null, 'Cabernet Sauvignon', 'red', null),
+  (null, 'Champagne', 'sparkling', null),
+  (null, 'Chardonnay', 'white', null),
+  (null, 'Malbec', 'red', null),
+  (null, 'Merlot', 'red', null),
+  (null, 'Pinot Grigio', 'white', null),
+  (null, 'Pinot Noir', 'red', null),
+  (null, 'Red Blend', 'red', null),
+  (null, 'Sauvignon Blanc', 'white', null),
+  (null, 'Syrah/Shiraz', 'red', null),
+  (null, 'Zinfandel', 'red', null);
